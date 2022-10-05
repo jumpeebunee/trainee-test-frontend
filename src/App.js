4325b3a4c93b56926bbeee8b0ea2589c4f801d0c
Ben <jumpeebunee@gmail.com>
2022-10-05 10:52:44 +0300

    feat: create navigation and fetching

--- src/App.js
+++ src/App.js
@@ -1,13 +1,33 @@
 import './styles/style.css';
-import { BrowserRouter } from "react-router-dom";
 import AppRouter from "./components/AppRouter";
+import { useState, useEffect } from 'react';
+import { BrowserRouter } from "react-router-dom";
+import { getAllUsers } from './context';
+import { useFetching } from './hooks/useFetching';
+import UsersService from "./API/UsersService";
 
 function App() {
+
+  const [users, setUsers] = useState(false);
+
+  const [fetchUsers] = useFetching(async () => {
+    const response = await UsersService.getAll();
+    setUsers(response.items);
+  });
+
+  useEffect(() => {
+    fetchUsers();
+  },[]);
+
   return (
-    <div className="App">
-      <BrowserRouter>
-        <AppRouter/>
-      </BrowserRouter>
+    <div className='app-wrapper'>
+      <div className='app'>
+        <getAllUsers.Provider value={{users, setUsers}}>
+            <BrowserRouter>
+              <AppRouter/>
+            </BrowserRouter>
+          </getAllUsers.Provider>
+      </div>
     </div>
   );
 }
--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -1,8 +1,31 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
+import { useContext } from "react";
+import { getAllUsers } from "../context";
+import { useFetching } from "../hooks/useFetching";
+import UsersService from "../API/UsersService";
+import Navigation from "../components/Navigation";
+
 
 const MainPage = () => {
+
+    const [totalUsers, setTotalUsers] = useState([]);
+    const {users, setlAllUsers} = useContext(getAllUsers);
+
+    const [fetchUsers, isLoading, isError] = useFetching(async () => {
+        const response = await UsersService.getAll();
+        setTotalUsers(response.items);
+    });
+
+    useEffect(() => {
+        (users === false) ? fetchUsers() : setlAllUsers(totalUsers);
+    },[]);
+
     return (
-        <h2>Я крутой</h2>
+        <div className="container">
+            <Navigation>
+
+            </Navigation>
+        </div>
     );
 };
 
