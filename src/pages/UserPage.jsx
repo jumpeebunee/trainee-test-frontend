f948dd910e7ffec67c5216269d040912ae5f7d5c
Ben <jumpeebunee@gmail.com>
2022-10-05 15:50:25 +0300

    feat: add validation for birthday and age

--- src/pages/UserPage.jsx
+++ src/pages/UserPage.jsx
@@ -4,20 +4,41 @@ import { getAllUsers } from "../context";
 import UserProfile from "../components/UserProfile";
 import departments from "../data/departments"
 import back from '../img/back.svg';
+import birthday from '../img/birthday.svg';
+import phone from '../img/phone.svg'
 
 const UserPage = () => {
 
     const { userId } = useParams();
     const {users, setlAllUsers} = useContext(getAllUsers);
     const [user, setUser] = useState([]);
+    const [userInfo, setUserInfo] = useState({age: '', phone: '', birthday: ''});
+
+    const declaration = ['год', 'года', 'лет'];
+    const cases = [2, 0, 1, 1, 1, 2];  
+
+    const getUserBirthday = () => {
+        const options = { month: 'long', day: 'numeric', year: 'numeric' };
+        const date = new Date(user.birthday);
+        let age = new Date().getFullYear() - date.getFullYear();
+        let caseAge = (age % 100 > 4 && age % 100 < 20) ? 2 : cases[(age%10<5)?age%10:5];
+
+        setUserInfo(
+            {
+                ...userInfo,
+                birthday: date.toLocaleDateString('ru-RU', options).slice(0, -2),
+                age: age.toString() + ` ${declaration[caseAge]}`
+            });
+    };
 
     useEffect(() => {
         let currentUser = null;
         if (users) {
             currentUser = users.find(user => user.id === userId);
             setUser(currentUser);
+            getUserBirthday();
         };
-    })
+    }, [users, user])
 
     if (!users) {
         return (
@@ -45,14 +66,14 @@ const UserPage = () => {
                     <ul className="user-page__list">
                         <li>
                             <div className="user-page__list-info">
-                                <span></span>
-                                <div>5 июня 1996</div>
+                                <span style={{backgroundImage: `url(${birthday})`}}></span>
+                                <div>{userInfo.birthday}</div>
                             </div>
-                            <div className="user-page__list-age">24 года</div>
+                            <div className="user-page__list-age">{userInfo.age}</div>
                         </li>
                         <li>
                             <div className="user-page__list-info">
-                                <span></span>
+                                <span style={{backgroundImage: `url(${phone})`}}></span>
                                 <div>+7 (999) 900 90 90</div>
                             </div>
                         </li>
