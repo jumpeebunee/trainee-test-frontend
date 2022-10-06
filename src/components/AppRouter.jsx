bbf4dff108d5f6d61aa17e4dfc894dd6aa4bd7fd
Ben <jumpeebunee@gmail.com>
2022-10-06 14:23:56 +0300

    feat: add route to main

--- src/components/AppRouter.jsx
+++ src/components/AppRouter.jsx
@@ -3,12 +3,12 @@ import { Routes, Route } from "react-router-dom";
 import MainPage from "../pages/MainPage";
 import UserPage from "../pages/UserPage";
 
-
 const AppRouter = () => {
     return (
         <Routes>
             <Route element={<MainPage/>} path="/"></Route>
             <Route element={<UserPage/>} path="/user/:userId"></Route>
+            <Route element={<MainPage/>} path="*"></Route>
         </Routes>
     );
 };
